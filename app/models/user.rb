9e730512dabada410014871b241dcbfc0fd73890
kento kadowaki <kadowakikento@gmail.com>
2022-07-13 23:39:02 +0900

    Userモデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 5 }
+end
