884b152d7b2826727a31dcda3bf0e667c4447ac6
kento kadowaki <kadowakikento@gmail.com>
2022-07-16 03:00:02 +0900

    adminのページとcrud機能の作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,56 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+  skip_before_action :login_already
+
+  def index
+    @users = User.all.order(:id)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザーを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tasks = @user.tasks
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "ユーザーを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザーを削除しました"
+    else  
+      redirect_to admin_users_path
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation, :admin)
+  end
+end
+
