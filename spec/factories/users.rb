2028a3350f5b7a03d775e16484557ea40565ac1d
kento kadowaki <kadowakikento@gmail.com>
2022-07-20 15:06:26 +0900

    テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,26 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
+    id { "0" }
+    name { "test_name" }
+    email { "test@test.com" }
+    password { "12345678" }
+    password_confirmation { "12345678"}
+    admin { false }
+  end
+  factory :second_user, class: User do
+    id { "1" }
+    name { "second_name" }
+    email { "second@second.com" }
+    password { "12345678" }
+    password_confirmation { "12345678"}
+    admin { true }
+  end
+  factory :third_user, class: User do
+    id { "2" }
+    name { "third_name" }
+    email { "third@third.com" }
+    password { "12345678" }
+    password_confirmation { "12345678"}
+    admin { true }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:all) do
+    FileUtils.rm_rf(Dir[Rails.root.join('tmp', 'screenshots', '*')], secure: true)
+  end
 end
