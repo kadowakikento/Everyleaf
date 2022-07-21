9891e972a346cd8f84035c83b88030f2fde495cf
kento kadowaki <kadowakikento@gmail.com>
2022-07-21 20:06:31 +0900

    ラベル機能の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
   validates :title, presence: true
   validates :deadline, presence: true
   validates :priority, presence: true
@@ -10,6 +12,4 @@ class Task < ApplicationRecord
   scope :status_title, -> (params) do where("title LIKE ?", "%#{params[:task][:title]}%").where(status: params[:task][:status]) end
   scope :title, -> (title) do where("title LIKE ?", "%#{title}%") end
   scope :status, -> (status) do where(status: status) end
-  # scope :title -> (column, :title_word) { where("#{column}like?", "%#{title_word}%")}
-  # Example: t.column "first_name", :string)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "kento", email: "sample@sample.com", password: "12345678")
+User.create(name: "kento", email: "sample@sample.com", password: "12345678", admin: true)
+
+10.times do |i|
+  Label.create(name: "label#{i + 1}")
+end
+
 # User.create(name: "ユーザー#{n + 1}", email: "test#{n + 1}@test.com", password_digest: "12345678")
 # end
