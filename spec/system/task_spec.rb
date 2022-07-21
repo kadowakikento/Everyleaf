82aac0b9521a93038407bd0401fc656aa94cce44
kento kadowaki <kadowakikento@gmail.com>
2022-07-22 01:04:47 +0900

    テストのコメントアウトを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,104 +1,104 @@
-# require 'rails_helper'
-# RSpec.describe 'タスク管理機能', type: :system do
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
 
-#   let!(:user) { FactoryBot.create(:user) }
-#   let!(:second_user) { FactoryBot.create(:second_user) }
-#   let!(:third_user) { FactoryBot.create(:third_user) }
-#   let!(:task) { FactoryBot.create(:task, user: user) }
-#   let!(:second_task) { FactoryBot.create(:second_task, user: user) }
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:second_user) { FactoryBot.create(:second_user) }
+  let!(:third_user) { FactoryBot.create(:third_user) }
+  let!(:task) { FactoryBot.create(:task, user: user) }
+  let!(:second_task) { FactoryBot.create(:second_task, user: user) }
 
-#   before do 
-#     visit new_session_path
-#     fill_in 'session_email', with: 'test@test.com'
-#     fill_in 'session_password', with: '12345678'
-#     click_button 'ログイン'
-#   end
+  before do 
+    visit new_session_path
+    fill_in 'session_email', with: 'test@test.com'
+    fill_in 'session_password', with: '12345678'
+    click_button 'ログイン'
+  end
 
-#   describe '新規作成機能' do
-#     context 'タスクを新規作成した場合' do
-#       it '作成したタスクが表示される' do
-#         visit new_task_path
-#         fill_in 'task_title',	with: '会食'
-#         fill_in 'task_content', with: '〇〇さんと会食'
-#         fill_in 'task_deadline',  with: Date.today
-#         find("#task_status").find("option[value='未着手']").select_option
-#         find("#task_priority").find("option[value='中']").select_option
-#         click_button '登録する'
-#         expect(page).to have_content '会食'
-#       end
-#     end
-#   end
-#   describe '一覧表示機能' do
-#     context '一覧画面に遷移した場合' do
-#       it '作成済みのタスク一覧が表示される' do
-#         visit tasks_path
-#         expect(page).to have_content 'test_title1'
-#       end
-#     end
-#   context 'タスクが作成日時の降順に並んでいる場合' do
-#     it '新しいタスクが一番上に表示される' do
-#       visit tasks_path
-#       task_list = all('.task_row')
-#       expect(task_list[0]).to have_content 'test_title1'
-#       expect(task_list[1]).to have_content 'test_title2'
-#     end
-#   end
-#   context '終了期限でソートした場合' do
-#     it '終了期限の降順で表示される' do
-#       visit tasks_path
-#       click_on "終了期限"
-#       sleep(0.5)
-#       task_list = all('.task_row')
-#       expect(task_list[0]).to have_content 'test_title1'
-#       expect(task_list[1]).to have_content 'test_title2'
-#     end 
-#   end
-#   context '優先順位でソートした場合' do
-#     it '優先順位の高い順で表示される' do
-#       visit tasks_path
-#       click_on "優先度"
-#       sleep(0.5)
-#       task_list = all('.task_row')
-#       expect(task_list[0]).to have_content 'test_title2'
-#       expect(task_list[1]).to have_content 'test_title1'
-#     end 
-#   end
-# end
-#   describe '詳細表示機能' do
-#     context '任意のタスク詳細画面に遷移した場合' do
-#       it '該当タスクの内容が表示される' do
-#         visit tasks_path
-#         click_link '詳細', match: :first
-#         expect(page).to have_content 'test_title1'
-#       end
-#     end
-#   end
-#   describe '検索機能' do
-#     before do
-#       visit tasks_path
-#     end
-#     context 'タイトルであいまい検索をした場合' do
-#       it '検索キーワードを含むタスクで絞り込まれる' do
-#         visit tasks_path
-#         fill_in 'task_title', with: 'test_title1'
-#         click_button '検索'
-#         expect(page).to have_content 'test_title1'
-#       end
-#     end
-#     context 'ステータス検索をした場合' do
-#       it 'ステータスに完全一致するタスクが絞り込まれる' do
-#         find("#task_status").find("option[value='未着手']").select_option
-#         click_button '検索'
-#         expect(page).to have_content '未着手'
-#       end
-#     end
-#     context 'タイトルのあいまい検索とステータス検索をした場合' do
-#       it '検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる' do
-#         fill_in 'task_title', with: 'test_title1'
-#         find("#task_status").find("option[value='未着手']").select_option
-#         click_button '検索'
-#         expect(page).to have_content '未着手'
-#       end
-#     end
-#   end
-# end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'task_title',	with: '会食'
+        fill_in 'task_content', with: '〇〇さんと会食'
+        fill_in 'task_deadline',  with: Date.today
+        find("#task_status").find("option[value='未着手']").select_option
+        find("#task_priority").find("option[value='中']").select_option
+        click_button '登録する'
+        expect(page).to have_content '会食'
+      end
+    end
+  end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        visit tasks_path
+        expect(page).to have_content 'test_title1'
+      end
+    end
+  context 'タスクが作成日時の降順に並んでいる場合' do
+    it '新しいタスクが一番上に表示される' do
+      visit tasks_path
+      task_list = all('.task_row')
+      expect(task_list[0]).to have_content 'test_title1'
+      expect(task_list[1]).to have_content 'test_title2'
+    end
+  end
+  context '終了期限でソートした場合' do
+    it '終了期限の降順で表示される' do
+      visit tasks_path
+      click_on "終了期限"
+      sleep(0.5)
+      task_list = all('.task_row')
+      expect(task_list[0]).to have_content 'test_title1'
+      expect(task_list[1]).to have_content 'test_title2'
+    end 
+  end
+  context '優先順位でソートした場合' do
+    it '優先順位の高い順で表示される' do
+      visit tasks_path
+      click_on "優先度"
+      sleep(0.5)
+      task_list = all('.task_row')
+      expect(task_list[0]).to have_content 'test_title2'
+      expect(task_list[1]).to have_content 'test_title1'
+    end 
+  end
+end
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
+        visit tasks_path
+        click_link '詳細', match: :first
+        expect(page).to have_content 'test_title1'
+      end
+    end
+  end
+  describe '検索機能' do
+    before do
+      visit tasks_path
+    end
+    context 'タイトルであいまい検索をした場合' do
+      it '検索キーワードを含むタスクで絞り込まれる' do
+        visit tasks_path
+        fill_in 'task_title', with: 'test_title1'
+        click_button '検索'
+        expect(page).to have_content 'test_title1'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it 'ステータスに完全一致するタスクが絞り込まれる' do
+        find("#task_status").find("option[value='未着手']").select_option
+        click_button '検索'
+        expect(page).to have_content '未着手'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it '検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる' do
+        fill_in 'task_title', with: 'test_title1'
+        find("#task_status").find("option[value='未着手']").select_option
+        click_button '検索'
+        expect(page).to have_content '未着手'
+      end
+    end
+  end
+end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,143 +1,143 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'ユーザー登録・セッション機能・管理画面のテスト', type: :system do
-#   describe 'ユーザー登録のテスト' do
-#     context 'ユーザーが登録がなく、ログインしていない場合' do
-#       it 'ユーザーが新規登録をすれば、詳細画面に移動する' do
-#         visit new_user_path
-#         fill_in 'user_name', with: 'test_name'
-#         fill_in 'user_email', with: 'test@test.com'
-#         fill_in 'user_password', with: '12345678'
-#         fill_in 'user_password_confirmation', with: '12345678'
-#         click_on 'ユーザー登録'
-#         expect(page).to have_content 'test_name'
-#       end
-#     end
-#     context 'ユーザーがログインせずにタスク一覧へ飛んだとき' do
-#       it '​ログイン画面に遷移する​' do
-#         visit tasks_path
-#         expect(current_path).to eq new_session_path
-#       end 
-#     end
-#   end
-#   describe 'セッション機能のテスト' do 
-#     before do
-#       @user = FactoryBot.create(:user)
-#       @second_user = FactoryBot.create(:second_user)
-#     end
-#     context 'アカウント登録済みのユーザがログインしようとした場合' do
-#       it 'ログインできる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'test@test.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         expect(current_path).to eq user_path(@user)
-#       end
-#       it '自分の詳細画面に飛べること' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'test@test.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         expect(current_path).to eq user_path(@user)
-#       end 
+RSpec.describe 'ユーザー登録・セッション機能・管理画面のテスト', type: :system do
+  describe 'ユーザー登録のテスト' do
+    context 'ユーザーが登録がなく、ログインしていない場合' do
+      it 'ユーザーが新規登録をすれば、詳細画面に移動する' do
+        visit new_user_path
+        fill_in 'user_name', with: 'test_name'
+        fill_in 'user_email', with: 'test@test.com'
+        fill_in 'user_password', with: '12345678'
+        fill_in 'user_password_confirmation', with: '12345678'
+        click_on 'ユーザー登録'
+        expect(page).to have_content 'test_name'
+      end
+    end
+    context 'ユーザーがログインせずにタスク一覧へ飛んだとき' do
+      it '​ログイン画面に遷移する​' do
+        visit tasks_path
+        expect(current_path).to eq new_session_path
+      end 
+    end
+  end
+  describe 'セッション機能のテスト' do 
+    before do
+      @user = FactoryBot.create(:user)
+      @second_user = FactoryBot.create(:second_user)
+    end
+    context 'アカウント登録済みのユーザがログインしようとした場合' do
+      it 'ログインできる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test@test.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        expect(current_path).to eq user_path(@user)
+      end
+      it '自分の詳細画面に飛べること' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test@test.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        expect(current_path).to eq user_path(@user)
+      end 
 
-#       it '一般ユーザーが他人の詳細画面に飛ぶと、タスク一覧画面に遷移する' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'test@test.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit user_path(@second_user)
-#         expect(current_path).to eq tasks_path
-#       end
+      it '一般ユーザーが他人の詳細画面に飛ぶと、タスク一覧画面に遷移する' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test@test.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit user_path(@second_user)
+        expect(current_path).to eq tasks_path
+      end
     
-#       it 'ログアウトができる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'test@test.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit user_path(@user)
-#         click_on 'ログアウト'
-#         expect(page).to have_content 'ログアウトしました'
-#       end
-#     end 
-#   end
-#   describe '管理画面のテスト' do
-#     before do
-#       @second_user = FactoryBot.create(:second_user)
-#       @user = FactoryBot.create(:user)
-#     end
-#     context '管理ユーザが管理画面にアクセスしようとするとき' do
-#       it '管理画面にアクセスできる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'second@second.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit admin_users_path
-#         expect(page).to have_content 'second@second.com'
-#       end
-#     end
-#     context '一般ユーザが管理画面にアクセスしようとするとき' do
-#       it '自分のタスクを表示させる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'test@test.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit admin_users_path
-#         expect(page).to have_content '管理者以外はアクセスできません'
-#       end
-#     end
-#     context '管理ユーザーがユーザー登録した場合' do
-#       it 'ユーザーの登録ができる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'second@second.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit new_admin_user_path
-#         fill_in 'user_name', with: 'test_user'
-#         fill_in 'user_email', with: 'test_user@user.com'
-#         fill_in 'user_password', with: '12345678'
-#         fill_in 'user_password_confirmation', with: '12345678'
-#         click_button 'ユーザー登録'
-#         visit admin_users_path
-#         expect(page).to have_content 'test_user'
-#       end
-#     end
-#     context '管理ユーザーがユーザーの詳細にアクセスした場合' do
-#       it 'ユーザーの詳細画面にアクセスできる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'second@second.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit admin_users_path
-#         visit user_path(0) 
-#         expect(current_path).to eq user_path(0)
-#       end 
-#     end
-#     context '管理ユーザーがユーザーの編集画面にアクセスした場合' do
-#       it 'ユーザーの編集ができる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'second@second.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit edit_admin_user_path(0)
-#         fill_in 'user_password', with: '12345678'
-#         fill_in 'user_password_confirmation', with: '12345678'
-#         click_button 'ユーザー登録'
-#         expect(page).to have_content 'second@second.com'
-#       end
-#     end
-#     context '管理ユーザーがユーザーを削除した場合' do
-#       it 'ユーザーの削除ができる' do
-#         visit new_session_path
-#         fill_in 'session_email', with: 'second@second.com'
-#         fill_in 'session_password', with: '12345678'
-#         click_button 'ログイン'
-#         visit admin_users_path
-#         page.accept_confirm do
-#           first(".btn-outline-danger").click
-#         end
-#         expect(page).not_to have_content 'test@test.com'
-#       end
-#     end  
-#   end
-# end
+      it 'ログアウトができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test@test.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit user_path(@user)
+        click_on 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+      end
+    end 
+  end
+  describe '管理画面のテスト' do
+    before do
+      @second_user = FactoryBot.create(:second_user)
+      @user = FactoryBot.create(:user)
+    end
+    context '管理ユーザが管理画面にアクセスしようとするとき' do
+      it '管理画面にアクセスできる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'second@second.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit admin_users_path
+        expect(page).to have_content 'second@second.com'
+      end
+    end
+    context '一般ユーザが管理画面にアクセスしようとするとき' do
+      it '自分のタスクを表示させる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test@test.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit admin_users_path
+        expect(page).to have_content '管理者以外はアクセスできません'
+      end
+    end
+    context '管理ユーザーがユーザー登録した場合' do
+      it 'ユーザーの登録ができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'second@second.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit new_admin_user_path
+        fill_in 'user_name', with: 'test_user'
+        fill_in 'user_email', with: 'test_user@user.com'
+        fill_in 'user_password', with: '12345678'
+        fill_in 'user_password_confirmation', with: '12345678'
+        click_button 'ユーザー登録'
+        visit admin_users_path
+        expect(page).to have_content 'test_user'
+      end
+    end
+    context '管理ユーザーがユーザーの詳細にアクセスした場合' do
+      it 'ユーザーの詳細画面にアクセスできる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'second@second.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit admin_users_path
+        visit user_path(0) 
+        expect(current_path).to eq user_path(0)
+      end 
+    end
+    context '管理ユーザーがユーザーの編集画面にアクセスした場合' do
+      it 'ユーザーの編集ができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'second@second.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit edit_admin_user_path(0)
+        fill_in 'user_password', with: '12345678'
+        fill_in 'user_password_confirmation', with: '12345678'
+        click_button 'ユーザー登録'
+        expect(page).to have_content 'second@second.com'
+      end
+    end
+    context '管理ユーザーがユーザーを削除した場合' do
+      it 'ユーザーの削除ができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'second@second.com'
+        fill_in 'session_password', with: '12345678'
+        click_button 'ログイン'
+        visit admin_users_path
+        page.accept_confirm do
+          first(".btn-outline-danger").click
+        end
+        expect(page).not_to have_content 'test@test.com'
+      end
+    end  
+  end
+end
